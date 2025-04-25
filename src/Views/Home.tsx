b33a4e32ac99eb60ba774af1444257634e29fcf9
traviicii <125103256+traviicii@users.noreply.github.com>
2025-04-25 18:31:30 -0400

    Reverted to hosted backend link

--- src/Views/Home.tsx
+++ src/Views/Home.tsx
@@ -176,7 +176,7 @@ const Home = () => {
         // frontend hosted 'https://recipe-sorter-back.onrender.com/parse-recipes'
 
         try {
-            const response = await fetch('http://127.0.0.1:8000/parse-recipes', {
+            const response = await fetch('https://recipe-sorter-back.onrender.com/parse-recipes', {
                 method: 'POST',
                 body: formData,
                 // Do NOT manually set the "Content-Type" header;
